1ee19fea7ac5b0b9599ea3ccad46e08612149be8
Kal-Elx <kallej1johansson@gmail.com>
2019-11-20 11:21:32 +0100

    Updated mnist data set.

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -1,5 +1,6 @@
+import csv
+import gzip
 from neural_net import *
-import mnist_loader
 
 
 def new(hidden_layer_architecture: List[int], epochs: int, mini_batch_size: int, learning_rate: float,
@@ -10,8 +11,7 @@ def new(hidden_layer_architecture: List[int], epochs: int, mini_batch_size: int,
                    regularization_technique=RegularizationTechnique.L2)
 
     # Train the neural net.
-    nn.train(training_set=training_data, epochs=epochs, mini_batch_size=mini_batch_size, learning_rate=learning_rate,
-             regularization=regularization_param, momentum_coefficient=momentum_coefficient, print_progress=True)
+    nn.train(training_set=training_data, epochs=epochs, mini_batch_size=mini_batch_size, learning_rate=learning_rate, print_progress=True)
 
     # Save the neural net to file.
     nn.save('examples/mnist-{0}.nn'.format(str(hidden_layer_architecture)).replace('[', '').replace(']', '')
@@ -27,28 +27,58 @@ def test(nn: NeuralNet, test_data: List[Tuple[np.ndarray, np.ndarray]]):
     for x, y in test_data:
         output = nn.exec(x)
         guess = 0
+        ans = 0
         for i in range(10):
             if output[i][0] > output[guess][0]:
                 guess = i
-        if guess != y:
-            print("Correct: {0}, Guess: {1}, Output: {2}".format(y, guess, str(output.flatten()).replace('\n', ' ')))
-        correct += (guess == y)
+            if y[i][0] > y[ans][0]:
+                ans = i
+        if guess != ans:
+            print("Correct: {0}, Guess: {1}, Output: {2}".format(ans, guess, str(output.flatten()).replace('\n', ' ')))
+        correct += (guess == ans)
 
     print("\nAccuracy: {0}%".format((correct / len(test_data) * 100)))
+
+
+def parse_mnist(input_file: str, output_file: str):
+    first = True
+    with open(input_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        data = []
+        for row in csv_reader:
+            if first:
+                first = False
+                continue
+
+            x = np.array([[float(x)/255.0] for x in row[1:]])
+            y = np.zeros((10, 1))
+            y[int(row[0])] = 1.0
+            data.append((x, y))
+
+            if len(data) % 1000 == 0:
+                print(len(data))
+
+    filehandler = gzip.open(output_file, 'wb')
+    pickle.dump(data, filehandler)
+    filehandler.close()
 
 
 if __name__ == "__main__":
 
     # Prepare the data set.
-    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-    training_data = list(training_data)
-    validation_data = list(validation_data)
-    test_data = list(test_data)
+    #parse_mnist('mnist_train.csv', 'training_data.pkl.gzip') # Files to large for git
+    filehandler = gzip.open('training_data.pkl.gzip', 'rb')
+    training_data = pickle.load(filehandler)
+    filehandler.close()
+
+    #parse_mnist('mnist_test.csv', 'test_data.pkl.gzip') # Files to large for git
+    filehandler = gzip.open('test_data.pkl.gzip', 'rb')
+    test_data = pickle.load(filehandler)
+    filehandler.close()
 
     # Create new neural net or load an existing one.
-    nn = new(hidden_layer_architecture=[100], epochs=30, mini_batch_size=10, learning_rate=0.1,
-             regularization_param=5.0, momentum_coefficient=0.8, training_data=training_data)
+    nn = new(hidden_layer_architecture=[30], epochs=30, mini_batch_size=10, learning_rate=3.0, training_data=training_data[:1000])
     # nn = NeuralNet.load('examples/mnist-100.nn')
 
     # Test the neural net.
-    test(nn=nn, test_data=test_data)
+    test(nn=nn, test_data=test_data[:100])
